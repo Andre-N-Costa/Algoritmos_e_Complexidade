a9aabe56df4f514bc284233e4e252eec79181206
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-03 19:33:10 +0000

    Update ficha_3.c

--- fichas/ficha_3.c
+++ fichas/ficha_3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 // 1 - MIN-HEAPS
 
@@ -15,98 +15,105 @@ void swap(int h[],int i,int m){
     h[i] = temp;
 }
 
-//2.
+int folha(int h[], int i){
+    while(h[left(i)] && h[right(i)]){
+        i++;
+    }
+return i;
+}
+
+//2-----------------------------
 void bubbleUp (int i, int h[]){
-    int m;
-    while (i > 0 && h[i] < h[pai(i)]){
+    while(h[pai(i)] > h[i] && h[pai(i)]){
         swap(h,i,pai(i));
         i = pai(i);
     }
 }
 
-//3.
+//3-------------------------------------
 void bubbleDown (int i, int h[], int N){
-    int m;
-    while (left(i) < N){
-        m = left(i);
-        if ((h[right(i)] < h[i]) && (h[right(i)] < h[left(i)])){
-            m = right(i);
+    while(left(i) < N){
+        if (h[i] > h[left(i)] && h[left(i)] < h[right(i)]) {
+            swap(h,i,left(i));
+            i = left(i);
         }
-        if (h[m] > h[i]) break;
-        swap(h,i,m);
-        i = m;
+        else if (h[i] > h[right(i)] && h[left(i)] > h[right(i)]){
+            swap(h,i,right(i));
+            i = right(i);
+        }
+        else break;
     }
 }
 
-void printmh(int minheap[]){
+void printmh(int minheap[],int N){
     int j = 0;
-    while (minheap[j] != -1){
+    while (j < N){
         printf("%d ", minheap[j]);
         j++;
     }
 }
 
+#define Max 100
 typedef struct pQueue {
     int valores [Max];
     int tamanho;
 } PriorityQueue;
 
-//4.
-
+//4---------------------------
 void empty (PriorityQueue *q){
     q->tamanho = 0;
 }
 
 int isEmpty (PriorityQueue *q){
-    return (q->tamanho == 0);
+    return(!q->tamanho);
 }
 
 int add (int x, PriorityQueue *q){
     if (q->tamanho == Max) return 1;
     q->valores[q->tamanho] = x;
     bubbleUp(q->tamanho,q->valores);
-    (q->tamanho)++;
+    q->tamanho++;
     return 0;
 }
 
-int remv (PriorityQueue *q, int *rem){
-    if (isEmpty(q)) return 1;
+int removepq (PriorityQueue *q, int *rem){
+    if (!q->tamanho) return 1;
     *rem = q->valores[0];
-    (q->tamanho)--;
-    int temp = q->valores[q->tamanho];
-    q->valores[0] = temp;
+    q->valores[0] = q->valores[q->tamanho - 1];
+    q->tamanho--;
     bubbleDown(0,q->valores,q->tamanho);
     return 0;
 }
-//5.
+
+//5-----------------------------
+void heapifyTD (int v[], int N){
+    for(int i = 0; i < N; i++){
+        bubbleUp(i,v);
+    }
+}
 
 void heapifyBU (int v[], int N){
-    for(int i = 0; i < N; i++){
-        if ((right(v[i]) || left(v[i]))) bubbleDown(v[i],v,N);
-    }
-}
-//complexidade : Nlog(N)
-
-void heapifyTD(int v[], int N){
-    for(int i = N - 1; i > 0; i--){
-        bubbleUp(v[i],v);
-    }
-}
-//complexidade : Nlog(N)
-
-void ordenaHeap (int h[], int N) {
-    printmh(h);
-    for (int i = N-1; i > 0; i--) {
-        swap(h, 0, i);
-        bubbleDown(0, h, i-1);
-        printf("\n");
-        printmh(h);
+    for(int i = N -1; i >= 0; i--){
+        bubbleDown(i,v,N);
     }
 }
 
+//6------------------------------
+void ordenaHeap (int h[], int N){
+    swap(h,0,N - 1);
+    for(int i = N - 2; i >= 0;i--){
+        bubbleDown(0,h,i);
+        swap(h,0,i);
+    }
+}
 
-//2 - TABELAS DE HASH
+//2 - Hash
 
+#define Size 100
+typedef struct nodo {
+    char *chave; int ocorr;
+    struct nodo *prox;
+} Nodo, *THash [Size];
 
 unsigned hash(char *str){
     unsigned hash = 5381;
@@ -116,60 +123,61 @@ unsigned hash(char *str){
     return hash;
 }
 
-//2.1 - CHAINING
-
-#define Size 100
-typedef struct nodo {
-    char *chave; int ocorr;
-    struct nodo *prox;
-} Nodo, *THash [Size];
-
-//1.
-void initEmpty (THash t){
-    for(int i = 0; i < Size; i++) t[i] = NULL;
+//1-----------------------
+void initEmptyH (THash t){
+    for(int i = 0; i < Size; i++){
+        t[i] = NULL;
+    }
 }
 
-//2.
+//2--------------------------
 void addH (char *s, THash t){
-    int h = hash(s) % Size;
-    Nodo *temp = t[h];
-    while (temp && strcmp(s,temp->chave)){
-        temp = temp->prox;
-    }
-    if (temp) temp->ocorr++;
-    else {
-        temp = malloc(sizeof(Nodo));
-        temp->chave = s;
-        temp->ocorr = 1;
-        temp->prox = t[h];
-        t[h] = temp;
-    }
-}
-
-//3.
-int lookup (char *s, THash t){
     int h = hash(s) % Size;
     Nodo *temp = t[h];
     while(temp && strcmp(s,temp->chave)){
         temp = temp->prox;
     }
-    if (!temp) return 0;
-    else return temp->ocorr;
+    if (temp) temp->ocorr++;
+    else{
+        temp = malloc(sizeof (Nodo));
+        temp->ocorr = 1;
+        strcpy(temp->chave,s);
+        temp->prox = t[h];
+    }
+    t[h] = temp;
 }
 
-//4.
+//3---------------------------
+int lookup (char *s, THash t){
+    int h = hash(s) % Size;
+    Nodo *temp = t[h];
+    while(temp && strcmp(s, temp->chave)){
+        temp = temp->prox;
+    }
+    if (temp) return temp->ocorr;
+    else return 0;
+}
+
+//4----------------------------
 int removeH (char *s, THash t){
     int h = hash(s) % Size;
-    Nodo *temp = t[h], *ant = NULL;
-    while (temp && strcmp(s,temp->chave)){
-        ant = temp;
+    Nodo *temp = t[h];
+    Nodo *temp0 = NULL;
+    while(temp && strcmp(s,temp->chave)){
+        temp0 = temp;
         temp = temp->prox;
     }
     if (!temp) return 1;
     else{
-        if (temp->ocorr > 1) return temp->ocorr--;
-        else {
-            if(ant) ant->prox = temp->prox;
+        if (temp->ocorr > 1) {
+            temp->ocorr--;
+            t[h] = temp;
+        }
+        else{
+            if (temp0) {
+                temp0->prox = temp->prox;
+                t[h] = temp0;
+            }
             else t[h] = temp->prox;
             free(temp);
         }
@@ -177,129 +185,92 @@ int removeH (char *s, THash t){
     return 0;
 }
 
-
-//2.2 - OPEN ADDRESSING
-
 #define Size 100
 #define Free 0
 #define Used 1
 #define Del 2
 typedef struct bucket {
+    int probC;
     int status; // Free | Used | Del
     char *chave; int ocorr;
 } THash [Size];
 
-//1.
-
+//1--------------------------
+//dificil
 int where (char *s, THash t){
     int h = hash(s) % Size;
     int i = 0;
     int del = -1;
-    while((i < Size) && ((t[h].status == 1 && strcmp(s,t[h].chave)) || t[h].status == 2)){
-        if (t[h].status == 2) del = h;
-        h = (h + 1) % Size;i++;
+    while(i < Size && ((strcmp(s,t[h].chave) && t[h].status == Used) || t[h].status == Del)){
+        if (t[h].status == Del) del = h;
+        h = (h + 1) % Size;
+        i++;
     }
-    if (t[h].status == 1) return h;
-    else {
-        if (del >= 0) return del;
-        else return h;
-    }
+    if (del != -1) return del;
+    else return h;
 }
 
-void initEmpty1 (THash t){
+//2------------------------
+void initEmptyH1 (THash t){
     for(int i = 0; i < Size; i++){
-        t[i].status == 0;
+        t[i].status = Free;
     }
 }
 
-int addH1 (char *s, THash t){
+void addH1 (char *s, THash t){
     int h = where(s,t);
-    if (t[h].status == 1 && !strcmp(s,t[h].chave)) {t[h].ocorr++;return 0;}
-    if (t[h].status == 1) return 1;
-    t[h].status = 1;
-    t[h].ocorr = 1;
-    t[h].chave = strdup(s);
-return 0;
+    if (t[h].status == Free){
+        t[h].status = Used;
+        strcpy(t[h].chave,s);
+        t[h].ocorr = 1;
+    }
+    else if (t[h].status == Used && !strcmp(t[h].chave,s)){
+        t[h].ocorr++;
+    }
 }
 
-int lookup1 (char *s, THash t){
+int lookupH1 (char *s, THash t){
     int h = where(s,t);
-    if (t[h].status == 1 && strcmp(s,t[h].chave)) return 0;
-    return t[h].ocorr;
+    if (t[h].status == Used && !strcmp(t[h].chave,s)) return t[h].ocorr;
+    return 0;
 }
 
 int removeH1 (char *s, THash t){
     int h = where(s,t);
-    if (t[h].status == 1 && strcmp(s,t[h].chave)) return 1;
-    t[h].status = 2;
+    if (t[h].status == Used && !strcmp(t[h].chave,s)){
+        if (t[h].ocorr > 1) t[h].ocorr--;
+        else t[h].status = Del;
+        return 0;
+    }
+return 1;
+}
+
+//3---------------------------
+int garb_collection (THash t){
+    THash newt;
+    char *keys[Size];
+    int ocor[Size];
+    int j = 0;
+    for(int i = 0; i < Size; i++){
+        if (t[i].status == Used){
+            keys[j] = strdup(t[i].chave);
+            ocor[j] = t[i].ocorr;
+            j++;
+        }
+    }
+    initEmptyH(newt);
+    for(int m = 0; keys[m]; m++){
+        while(ocor[m] > 0) {
+            addH(keys[m], newt);
+            ocor[m]--;
+        }
+    }
     return 0;
 }
 
-void copy(char *key,int times, THash t){
-    int pos;
-    for(pos = where(key,t); t[pos].status == 1; pos = (pos + 1) % Size)
-        ;
-        strcpy(t[pos].chave, key);
-        t[pos].ocorr = times;
-        t[pos].status = 1;
-}
 
-int garb_collection (THash t){
-    char *keys[Size];
-    int pos = 0,times[Size];
-    for(int i = 0; i < Size;i++){
-        if (t[i].status == 1) {
-            strcpy(keys[pos], t[i].chave);
-            times[pos] = t[i].ocorr;
-            pos++;
-        }
-    }
-    initEmpty1(t);
-    for(int i = 0; i < pos; i++){
-        copy(keys[i],times[i],t);
-    }
-return 0;
-}
 
 // MAIN -------------------------------------------------------
 
-int main(){
-
-    int minheap[Max];
-    int i = 0;
-    int x;
-    while (x != -1){
-        printf("input:");
-        scanf("%d",&x);
-        minheap[i] = x;
-        i++;
-    }
-    
-
-    /*----------TESTE BUBBLEUP-----------
-    minheap[i - 1] = 13;
-    minheap[i] = -1;
-    i++;
-    bubbleUp(i-2,minheap);
-    */
-    /*----------TESTE BUBBLEDOWN----------
-    bubbleDown(2,minheap,17);
-    */
-    /*----------TESTE ADD---------------
-    PriorityQueue heap;
-    heap.tamanho = 17;
-    for(int f = 0; f < heap.tamanho; f++) heap.valores[f] = minheap[f];
-    heap.valores[heap.tamanho + 1] = -1;
-    add(12,&heap);
-    */
-    /*----------TESTE REMOVE------------
-    PriorityQueue heap;
-    heap.tamanho = 17;
-    for(int f = 0; f < heap.tamanho; f++) heap.valores[f] = minheap[f];
-    heap.valores[heap.tamanho] = -1;
-    int r;
-    remv(&heap, &r);
-     */
-    /*----------TESTE HEAPSORT------------*/
-    ordenaHeap(minheap,7);
+int main() {
 }
